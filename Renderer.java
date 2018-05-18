bca7a9b474f0049d5e56e0663868400fade64ced
kelp1470 <36962706+kelp1470@users.noreply.github.com>
2018-05-18 10:52:21 -0500

    Add files via upload

--- Renderer.java
+++ Renderer.java
@@ -7,6 +7,7 @@ public class Renderer extends JPanel{
 	
 	private static final long serialVersionUID = 1L; 
 
+	@Override
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		
